f88cef5c8430d2beda10a20f39d3ade48e7238b7
Lorxen <brianylucia@gmailcom>
2023-02-12 17:40:06 +0100

    Windows update

--- Calculator.py
+++ Calculator.py
@@ -16,42 +16,68 @@ def mult(num1,num2):
 def div(num1,num2):
     return num1 / num2
 
+def is_number(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
 
 operation = []
 result = 0
 exit = False
 next = ''
+inp = True
 
 while exit == False:
 
     n = input('Please enter an operation with +, -, * or / separate with spaces: ')
 
     operation = n.split()
-    operate = True
+
+    if operation != [] and is_number(operation[0]) and is_number(operation[2]) and (operation[1] == '+' or operation[1] == '-' or operation[1] == '*' or operation[1] == '/'):
+        operate = True
+
+    else: 
+        operate = False
+        print('NOT VALID INPUT')
 
     while operate == True:
 
         if operation[1] == '+':
-            result = int(operation[0]) + int(operation[2])
+            result = float(operation[0]) + float(operation[2])
 
         elif operation[1] == '-':
-            result = int(operation[0]) - int(operation[2])
+            result = float(operation[0]) - float(operation[2])
 
         elif operation[1] == '*':
-            result = int(operation[0]) * int(operation[2])
+            result = float(operation[0]) * float(operation[2])
 
-        else: result = int(operation[0]) / int(operation[2])
+        else: result = float(operation[0]) / float(operation[2])
 
         print(result)
-        next = input('(+  -  *  /)  CLEAR  EXIT : ').lower()
+        contin = True
 
-        if next == 'exit':
-            exit = True
-            operate = False
-        
-        elif next == 'clear':
-            operate = False
+        while contin == True:
 
-        else:
-            operation = next.split()
-            operation.insert(0,result)
+            next = input('(+  -  *  /)  CLEAR  EXIT : ').lower()
+
+            if next == 'exit':
+                exit = True
+                operate = False
+                contin = False
+            
+            elif next == 'clear':
+                operate = False
+                contin = False
+
+            else:
+                operation = next.split()
+                if operation != [] and (operation[0] == '+' or operation[0] == '-' or operation[0] == '*' or operation[0] == '/') and is_number(operation[1]):
+                    contin = False
+                    operation.insert(0,result)
+
+                else: 
+                    print('NOT VALID INPUT')
+                    
