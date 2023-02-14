337116aeb626019a5caf637cbffb594d04f3bfaf
Lorxen <brianylucia@gmailcom>
2023-02-14 20:49:00 +0100

    windows update

---
+++ interest_payment_calculator.py
@@ -0,0 +1,36 @@
+# Collect the necessary inputs: principal, apr, years
+# Calculate the monthly payment
+# Show to the user
+
+def main():
+
+    while True:
+        try:
+            principal = float(input('Input the lian amount: '))
+            apr = float(input('Input the annual interest rate: '))
+            years = int(input('Input amount of yars: '))
+            break
+        except:
+            print('\nNOT VALID INPUT\n')
+
+    monthly_interest_rate = apr / 1200
+    amount_of_months = years * 12
+    monthly_payment = principal * monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-amount_of_months))
+
+    print('\nThe monthly payment for this loan is: %.2f' % monthly_payment + ' $')
+    print()
+
+
+print('\tThis is a monthly payment loan calculator \n')
+while True:
+    main()
+    correct = True
+    while correct == True:
+        inp = input('Restart or Exit?  R/E  ').lower()
+        print('')
+        if inp == 'e':
+            quit()
+        elif inp == 'r':
+            correct = False
+        else:
+            print('\n NOT VALID INPUT')
---
+++ random_pasword.py
@@ -0,0 +1,39 @@
+# Ask user if they want to generate a passowrd or not
+# If yes, ask for password lenght
+# Generate password
+# Print password
+# If initial response is no, exit program
+
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + '!@#$%^&*()')
+
+def generate_password():
+
+    while True:
+        try:
+            pas_len = int(input('\nHow long would you like your password to be? '))
+            break
+        except:
+            print('\nNOT VALID INPUT')
+    
+    pas = []
+    random.shuffle(characters)
+
+    for i in range(pas_len):
+        pas.append(random.choice(characters))
+
+    random.shuffle(pas)
+    pasw = ''.join(pas)
+    print('\n' + pasw + '\n')
+
+while True:
+
+    option = input('Do you want to generate a password?  Y/N  ').lower()
+    if option == 'y':
+        generate_password()
+    elif option == 'n':
+        quit()
+    else:
+        print('\nNOT VALID INPUT\n')
