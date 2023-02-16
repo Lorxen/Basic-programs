c2e85bb36f3946a067e055158f432c02b88f0f86
Lorxen <brianylucia@gmailcom>
2023-02-16 21:25:19 +0100

    windows

---
+++ graph_plotter.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+x1 = [2, 4, 5]
+y1 = [2, 3, 6]
+
+plt.plot(x1, y1, label = 'Line 1')
+
+x2 = [1, 4, 7]
+y2 = [1, 2, 3]
+
+plt.plot(x2, y2, label = 'Line 2')
+
+plt.xlabel('X Axis')
+
+plt.ylabel('Y Axis')
+
+plt.title('Demo Graph - Two lines')
+
+plt.legend()
+
+plt.show()
